c24b4f969c033673362430b35e7aa86a754354df
dmrfrht <dmrfrht1@gmail.com>
2021-01-22 22:36:05 +0300

    redis client connection the singleton way

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const createError = require('http-errors')
 require('dotenv').config()
 require('./helpers/init_mongodb')
 const { verifyAccessToken } = require('./helpers/jwt_helper')
+const client = require('./helpers/init_redis')
+
+client.SET("foo", "bar")
+client.GET('foo', (err, value) => {
+  if (err) console.log(err)
+  console.log(value)
+})
 
 const AuthRoute = require('./routes/Auth.route')
 
